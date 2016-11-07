78e6f3fd7483e1253c270f241b25fea7ca745bd4
chinhvm <chinhvm.uet.1995@gmail.com>
2016-11-07 23:53:31 +0700

    final update bt3

--- VuMinhChinh/BT3/src/chinhvm/FindSpecialYear.java
+++ VuMinhChinh/BT3/src/chinhvm/FindSpecialYear.java
@@ -5,11 +5,7 @@ public class FindSpecialYear {
 	int y2;
 
 	public FindSpecialYear(int year1, int year2) {
-		// TODO Auto-generated constructor stub
-		if(year1 < 0 || year2 < 0 || year1 > year2) {
-			throw new IllegalArgumentException("Invalid Input, check again!!!");
-		} 
-		
+		// TODO Auto-generated constructor stub		
 		this.y1 = year1;
 		this.y2 = year2;
 	}
@@ -28,10 +24,5 @@ public class FindSpecialYear {
 		}
 		
 		return count;
-	}
-	
-	public static void main(String[] args) {
-		FindSpecialYear y = new FindSpecialYear(1900, 2015);
-		System.out.println(y.findYear());
 	}
 }
